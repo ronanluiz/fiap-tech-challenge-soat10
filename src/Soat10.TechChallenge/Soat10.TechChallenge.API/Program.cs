0d352864ee95fd8d753806f8f0dc333f1fb954c9
Ronan Luiz Araújo <ronanluiz@gmail.com>
2025-01-04 14:02:37 -0300

    feat: Alteração da conexão do banco de dados para utilização de variáveis de ambiente

--- src/Soat10.TechChallenge/Soat10.TechChallenge.API/Program.cs
+++ src/Soat10.TechChallenge/Soat10.TechChallenge.API/Program.cs
@@ -4,12 +4,27 @@ using Soat10.TechChallenge.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var rootPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../.."));
+var envFilePath = Path.Combine(rootPath, ".env");
 
+if (File.Exists(envFilePath))
+{
+    DotNetEnv.Env.Load(envFilePath); // Carregar o arquivo .env
+    Console.WriteLine($"Arquivo .env carregado do caminho: {envFilePath}");
+}
+else
+{
+    Console.WriteLine($"Arquivo .env não encontrado no caminho: {envFilePath}");
+}
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                   .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+                   .AddEnvironmentVariables();
+
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 ApplicationBootstrapper.Register(builder.Services);
 InfrastructureBootstrapper.Register(builder.Services, builder.Configuration);
 
