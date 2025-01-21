9733572a1e216b36a5c7adc00b2fe750102ba0dd
DiogoBrandon <d_bb2001@hotmail.com>
2025-01-21 00:11:28 -0300

    Ajuste na mensagem de erro

--- src/Soat10.TechChallenge/Soat10.TechChallenge.Application/UseCases/CustomerRegistration/CustomerRegistrationRequestValidator.cs
+++ src/Soat10.TechChallenge/Soat10.TechChallenge.Application/UseCases/CustomerRegistration/CustomerRegistrationRequestValidator.cs
@@ -18,7 +18,7 @@ namespace Soat10.TechChallenge.Application.UseCases.CustomerRegistration
                 .NotEmpty().WithMessage("É preciso preencher o campo 'CPF'.")
                 .Length(11).WithMessage("CPF deve conter exatos 11 caracteres.")
                 .Matches(@"^\d+$").WithMessage("CPF deve conter apenas números.")
-                .Must(Validate).WithMessage("CPF está inválido"); ;
+                .Must(Validate).WithMessage("Não foi possível validar o CPF. Verificação inválida"); // Esse erro ocorre quando o dígito verificador do CPF é inválido.
         }
 
         private static bool Validate(string cpf)
