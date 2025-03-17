c00dd4f6ceda49705b8f1286b78e841cd9155818
DiogoBrandon <d_bb2001@hotmail.com>
2025-03-16 22:03:07 -0300

    Retirada de um using nao usado

--- src/Soat10.TechChallenge.Application/UseCases/GetStatusOrders/IGetStatusOrders.cs
+++ src/Soat10.TechChallenge.Application/UseCases/GetStatusOrders/IGetStatusOrders.cs
@@ -1,6 +1,4 @@
-﻿using Soat10.TechChallenge.Application.UseCases.GetOrders;
-
-namespace Soat10.TechChallenge.Application.UseCases.GetStatusOrders
+﻿namespace Soat10.TechChallenge.Application.UseCases.GetStatusOrders
 {
     public interface IGetStatusOrders
     {
